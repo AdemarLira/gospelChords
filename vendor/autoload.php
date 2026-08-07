a995a6dd7991724618f25b39da3dafd47585700f
AdemarLira <ademarliraneto@gmail.com>
2026-08-06 23:36:11 -0300

    Reajustando projeto

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitbaf0b1b659e688a64051c5ee9742a77e::getLoader();
+return ComposerAutoloaderInit43f04f8ab298ae680223494104e06cf9::getLoader();
